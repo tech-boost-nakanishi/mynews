f737e120ef32aab8af876bb34580797dbe36b415
”名前” <>
2020-02-08 02:00:23 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,21 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
 });
+
+
+/*
+    「http://XXXXXX.jp/XXX というアクセスが来たときに、
+    AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。
+*/
+Route::get('XXX', 'AAAController@bbb');
+
+
+/*
+  【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。
+  web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、
+  admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。  
+*/
+Route::group(['prefix' => 'admin'], function() {
+   Route::get('profile/create', 'ProfileController@add');
+   Route::get('profile/edit', 'ProfileController@edit');
+});
