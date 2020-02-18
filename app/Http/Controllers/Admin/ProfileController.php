d269bf49cb8373f868ff1948e889b3588939d26e
”名前” <>
2020-02-18 03:21:57 +0000

    First commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
@@ -14,6 +16,19 @@ class ProfileController extends Controller
     
     public function create(Request $request)
     {
+        // Varidationを行う
+        $this->validate($request, Profile::$rules);
+        
+        $profile = new Profile;
+        $form = $request->all();
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+      
         return redirect('admin/profile/create');
     }
     
